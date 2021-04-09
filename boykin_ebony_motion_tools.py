ed6f2f03d8bef49c731d9f28060461688ec8c27d
Ebony B <70521133+s10954717@users.noreply.github.com>
2021-04-09 13:58:18 -0400

    Version 0.0
    Finished?

---
+++ boykin_ebony_motion_tools.py
@@ -0,0 +1,13 @@
+# Speed and Acceleration Calculator, Ebony Boykin, 4/9/21
+
+
+
+
+time = float(input("Enter the time in seconds and press enter.\n"))
+distance = int(input("Enter the distance in meters and press enter.\n"))
+
+average_speed = distance / time
+
+
+
+print(f"The average speed was {average_speed} m/s.\n")
